a1211f7d0a476adeec675691c664654dada7bd6b
cawley <47256354+cawley@users.noreply.github.com>
2023-05-19 15:04:29 -0400

    GA for 8 Queens, then N as param

--- n_queens.py
+++ n_queens.py
@@ -1,8 +1,6 @@
-import utils as utils
 import math as math
 import random as rand
 
-#HELPER FUNCTION TO DETERMINE CONFLICTS IN TWO POSITIONS
 def conflict(row1, col1, row2, col2):
     """Would putting two queens in (row1, col1) and (row2, col2) conflict?"""
     
@@ -11,18 +9,7 @@ def conflict(row1, col1, row2, col2):
             row1 - col1 == row2 - col2 or   # same \ diagonal
             row1 + col1 == row2 + col2)     # same / diagonal
 
-#FITNESS EVALUATION FUNCTION. WE ARE MINIMIZING NUM_CONFLICTS THEREBY CHOOSING THE 
-#STATE THAT HAS THE LOWEST CORRESPONDING OBJECTIVE VALUE
-#INPUT: STATE 1D LIST OUTPUT: SCORE ASSOCIATED WITH A SPECIFIC STATE
-def fitness(state):
-    num_conflicts = 0
-    N = len(state)
-    for c1 in range(N):
-        for c2 in range(c1 + 1, N):
-            num_conflicts += conflict(state[c1], c1, state[c2], c2)
-    return (28 - num_conflicts)
-
-def min_h(state):
+def conflicts(state):
     num_conflicts = 0
     N = len(state)
     for c1 in range(N):
@@ -30,7 +17,19 @@ def min_h(state):
             num_conflicts += conflict(state[c1], c1, state[c2], c2)
     return num_conflicts
 
-#NOTE REMOVE ASEXUAL REPRODUCTION
+def max_conflicts(state):
+    for i in state:
+        state[i] = 0
+    return conflicts(state)
+
+def fitness(state):
+    num_conflicts = 0
+    N = len(state)
+    for c1 in range(N):
+        for c2 in range(c1 + 1, N):
+            num_conflicts += conflict(state[c1], c1, state[c2], c2)
+    return (max_conflicts(state) - num_conflicts)
+
 def roulette_selection(population, scores):
     population.sort(key = fitness, reverse = True)
     aggregate = 0.0
@@ -44,14 +43,14 @@ def roulette_selection(population, scores):
     r = rand.uniform(0, 1)
     for i in range (len(population)):
         if r < p_state[i]:
-            return population[i]
+            temp = population[i]
+            return temp
 
 def breed(p1, p2):
     pivot = rand.randint(1, len(p1) - 2)
     c1 = p1[:pivot] + p2[pivot:]
     c2 = p2[:pivot] + p1[pivot:]
     return [c1, c2]
-
 
 def swapmutation(c1, r_mut):
     r = rand.uniform(0, 1)
@@ -63,9 +62,6 @@ def swapmutation(c1, r_mut):
             c1[0][rpos2] = temp
     return c1
 
-#HERE IS EMPLOYED RANDOM RESET MUTATION BECAUSE IT SEEMS THAT SWAPMUTATION IS
-#NOT SUITABLE FOR THIS [IMPLEMENTATION]
-#INPUT IS A TUPLE
 def random_reset_mutation(c1, r_mut):
     for i in range (len(c1)):
         r = rand.uniform(0, 1)
@@ -73,10 +69,10 @@ def random_reset_mutation(c1, r_mut):
             new_gene = rand.randint(0, (len(c1) - 1))
             c1[i] = new_gene
     return c1
-    
+
 def genetic_algorithm(population, r_mut, n_iter):
     [best, score] = population[0], fitness(population[0])
-    h = min_h(population[0])
+    h = conflicts(population[0])
     for gen in range(n_iter):
         scores = [fitness(population[gen]) for gen in range(len(population))]
         #check for new best
@@ -87,7 +83,7 @@ def genetic_algorithm(population, r_mut, n_iter):
         parents = [roulette_selection(population, scores) for _ in range(len(population))]
         children = list()
         for i in range(0, len(parents) - 1, 2):
-            p1, p2 = parents[i], parents[i+1]
+            p1, p2 = parents[i], parents[i+1] 
             c1, c2 = breed(p1, p2)
             c1 = random_reset_mutation(c1, r_mut)
             c2 = random_reset_mutation(c2, r_mut)
@@ -95,26 +91,34 @@ def genetic_algorithm(population, r_mut, n_iter):
             children.append(c2)
         population = children
         for i in range(len(population)):
-            if min_h(population[i]) < h:
+            if conflicts(population[i]) < h:
                 h = min_h(population[i])
     print("\n", "SCORE: ", score, "\n")
     return [best, score, h]
 
 def main():
-    r_mut = 0.15
-    n_iter = 100
-    population = [[rand.randint(0, 7) for _ in range(8)] for _ in range(4)]
+    N = int(input("N QUEENS DEMO: Input desired dimension (N).\n"))
+    size = int(input("Population size"))
+    r_mut = float(input("Input Parameters: Mutation rate (default = 1/population size)"))
+    n_iter = int(input("Number of Iterations"))
 
-    total = 0
-    totalh = 0
-    for _ in range(1000):
-        best, score, temp = genetic_algorithm(population, r_mut, n_iter)
-        totalh += temp
-        total += score
+    population = [[rand.randint(0, N-1) for _ in range (N)] for _ in range(size)]
+    state = population[0]
+    score = 0
+    h = conflicts(state)
+
+    best_state = state
+    best_score = score 
+    best_h = h
+    for _ in range(n_iter):
+        best, score, h = genetic_algorithm(population, r_mut, n_iter)
+        if score > best_score:
+            best_score = score 
+            best_state = state4
+
+            best_h = h
     
-    print("\n", totalh/1000, "\n")
-
-    print(28 - (total/1000))
+    print("Optimal State: ", best_state, "Score: ", score, "H: ", best_h)
 
 
 
