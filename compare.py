eb03e0f109ab3e09b5b9116653e0c1b82508cb84
Gabe Ronan <gabrielpronan@gmail.com>
2023-05-23 15:37:18 -0400

    Added plot for GA

--- compare.py
+++ compare.py
@@ -14,11 +14,14 @@ def main():
     convInfoSARR = n_queens_SARR.repeatSARR(maxItr, numLoops, initialState, 100, 100)
 
     popSize = 4
-
+    convInfoGA = n_queens_GA.repeatGA(maxItr, numLoops, population=[initialState.copy() for _ in range(popSize)], numRuns=4000)
     convInfoGAv2 = n_queens_GAv2.repeatGAv2(maxItr, numLoops, population=[initialState.copy() for _ in range(popSize)], N=N, populationSize=popSize, numRuns=4000)
 
-    plt.plot(convInfoSARR[:, 0], convInfoSARR[:, 1], label='Steepest Ascent with Random Restart')
-    plt.plot(convInfoGAv2[:, 0], convInfoGAv2[:, 1], label='Genetic Algorithm V2')
+    line1, = plt.plot(convInfoSARR[:, 0], convInfoSARR[:, 1], label='Steepest Ascent with Random Restart')
+    line2, = plt.plot(convInfoGA[:, 0], convInfoGA[:, 1], label='Genetic Algorithm')
+    line3, = plt.plot(convInfoGAv2[:, 0], convInfoGAv2[:, 1], label='Genetic Algorithm V2')
+    plt.legend(handles=[line1, line2, line3])
+
     plt.show()
 
     return
