d16b9ebfc9108c70d7d77d9c852bba5608bdcebe
Liam Cawley <47256354+cawley@users.noreply.github.com>
2023-05-24 16:50:47 -0400

    Update n_queens_PSO.py
    Why the FUCK does pbest not update

--- n_queens_PSO.py
+++ n_queens_PSO.py
@@ -2,7 +2,7 @@ import math as math
 import random as rand
 import numpy as np
 
-out = open('out.txt', 'w')
+out = open('out.txt', 'w') 
 
 def conflict(row1, col1, row2, col2):
     """Would putting two queens in (row1, col1) and (row2, col2) conflict?"""
@@ -36,60 +36,61 @@ def fitness(state_in):
     return (max_conflicts(state) - num_conflicts)
 
 def pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia):
-    # X || V meaning they are identically indexed and represent the same population of particles
+    # X || V meaning they are identically indexed and make up the same population of particles
     x = [[(rand.randint(0, n_sub - 1)) for _ in range(n_sub)] for _ in range(n_parts)]
+    xprev = x.copy()
     v = [[(rand.randint(0, r_maxv)) for _ in range(n_sub)] for _ in range(n_parts)]
 
     # global inits
     best = x[0]
     best_score = fitness(best)
-    best_idx = 0
     gbest = best
     gbest_score = fitness(gbest)
     # local best inits
-    pbest = [0] * n_parts
+    pbest = [[0] * n_sub for _ in range(n_parts)]
     scores = []
 
     c = 0
     print("\n")
     while (best_score < max_conflicts(x[0])) and (c < n_iter):
         # calculate scores to compare
-        scores = [fitness(x[i]) for i in range(n_parts)]
         print("Iteration:", c)
-        for i in range(len(scores)):
+
+        for i in range(n_parts):
             for j in range(len(v[i])):
                 r = rand.randint(0, 2)
-                v[i][j] = inertia * v[i][j] + cognitive * r * (pbest[i] - x[i][j]) + social * r * (x[pbest.index(max(pbest))][j] - x[i][j])
+                v[i][j] = inertia * v[i][j] + cognitive * r * (pbest[i][j] - x[i][j]) + social * r * (x[pbest.index(max(pbest))][j] - x[i][j])
                 # v[i][j] is the velocity of particle i in dimension j, 
                 # x[i][j] is the current position of particle i in dimension j, 
                 # pbest[i][j] is the personal best position of particle i in dimension j, 
                 # gbest[j] is the global best position in dimension j, 
                 # w is the inertia weight, c1 and c2 are cognitive and social coefficients, respectively,
                 # rand() is a random number between 0 and 1.
-                # NOTE: For this problem (NQUEENS) the solution space has dimension 1 and thus
-                # there is no possibility to include a pbest or gbest for separate dimensions.
-                # NOTE: Well, you could make it a multidimensional (N-dimensional, N as in NQUEENS) 
-                # but this is futile and provides no computational benefit.
                 x[i][j] += v[i][j]
                 x[i][j] = x[i][j] % n_sub
         
-        # update personal best of each particle
-        for i in range(len(scores)):
-            if scores[i] > pbest[i]:
-                pbest[i] = scores[i]
-
-        #update current best
-        best = x[scores.index(max(scores))]
-        best_score = fitness(best)
-
-        # update gbest
-        gbest_score = max(pbest)
-        gbest = x[pbest.index(max(pbest))]
+        scores = [fitness(x[i]) for i in range(n_parts)]
+        pbest_scores = [fitness(pbest[i]) for i in range(n_parts)]
         
+        # update current best
+        best = x[scores.index(max(scores))]
+        best_score = max(scores)
+
+        # update pbest list (highest score across a certain index)
+        pbest[:] = [x[i] for i in range(len(pbest)) if fitness(pbest[i]) <= fitness(x[i])]
+
+        # update global best (best of the pbest)
+        if best_score > max(pbest_scores): 
+            gbest = best 
+            gbest_score = best_score
+        else:
+            gbest = pbest[pbest_scores.index(max(pbest_scores))]
+            gbest_score = fitness(pbest[pbest_scores.index(max(pbest_scores))])     
         print("X:")
         for i in range(len(x)):
-            print(x[i], scores[i], pbest[i])
-        print("Scores Max:", max(scores), scores.index(max(scores)))
+            print(x[i], scores[i], pbest[i], fitness(pbest[i]))
+        print("PBEST Max:", pbest[pbest_scores.index(max(pbest_scores))]) 
+        print("Scores Max:", max(scores), "Max Score IDX", scores.index(max(scores)))
         print("Best State:", best, "Best Score:", best_score)
         print("Global Best:", gbest, fitness(gbest),"\n")
         c+=1
@@ -104,9 +105,9 @@ def main():
     inertia = 1   #W
 
     #MAKE PARTICLES, SUB PARTICLE RANGE (0, N_SUB)
-    n_sub = 8   #BOARD SIZE (N in NQUEEN)
-    n_parts = 8 #POPULATION SIZE 
-    r_maxv = 2
+    n_sub = 4   #BOARD SIZE (N in NQUEEN)
+    n_parts = 4 #POPULATION SIZE 
+    r_maxv = 3
     n_iter = 10
 
     g = pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia)
