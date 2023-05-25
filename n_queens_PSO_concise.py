0bf2094edc5bc2fd3c504d545664fdd9f4258b9a
Liam Cawley <47256354+cawley@users.noreply.github.com>
2023-05-25 14:43:36 -0400

    Update n_queens_PSO_concise.py
    input for params

--- n_queens_PSO_concise.py
+++ n_queens_PSO_concise.py
@@ -15,14 +15,14 @@ global gbest
 global gbest_score
 
 def pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia):
-    gbest = [-1] * n_sub 
+    gbest = [-1]*n_sub 
     gbest_score = -np.inf
 
     particles = [{'states': [rand.randint(0, n_parts-1) for _ in range(n_sub)],
-                  'pbest': [],
+                  'pbest': [-1]*n_sub,
                   'pbest_score': 0,
                   'v': [rand.randint(0, r_maxv) for _ in range(n_sub)]} for _ in range(n_sub)]
-
+    h = conflicts(particles[0]['states'])
     for i in range(n_iter):
         for particle in particles:
             score = fitness(particle['states'])
@@ -40,25 +40,37 @@ def pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia):
                     r = rand.randint(0, 2)
                     particle['states'][state_idx] = inertia * particle['v'][state_idx] + cognitive * r * (particle['pbest'][state_idx] - state_val) + social * r * (gbest[state_idx] - state_val)
                     particle['states'][state_idx] = (particle['states'][state_idx] + particle['v'][state_idx]) % n_sub
-
         
+        for j in range(len(particles)):
+            if conflicts(particles[j]['states']) < h:
+                h = conflicts(particles[j]['states'])
+            print(particles[j])
 
-    return gbest
+        print(f"Global Best: {gbest} Score: {gbest_score}\n")
+
+    return gbest, h
 
 def main():
-    n_sub = 4
-    n_parts = 4
-    r_maxv = 3
-    n_iter = 10
+    n_sub = int(input("N (Queens)")) 
+    n_parts = int(input("Swarm Quantity"))
+    r_maxv = int(input("Max Velocity"))
+    n_iter = int(input("Iteration Count"))
+    n_samp = int(input("Amount of Trials"))
 
     cognitive = 1
     social = 1
-    inertia = 1
+    inertia = 2
 
-    g = pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia)
+    g, h = pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia)
 
-    print(conflicts([0]*len(g)))
-    print(fitness(g))
+    print("Max Score Possible for N queens", conflicts([0]*len(g)))
+    print("Best State:", g, "Best State Score:", fitness(g))
+
+    totalh = 0
+    for i in range(n_samp):
+        g, h = pso(n_sub, n_parts, r_maxv, n_iter, cognitive, social, inertia)
+        totalh += h
+    print(f"H Score: {totalh/n_samp}")
 
 if __name__ == "__main__":
     main()
