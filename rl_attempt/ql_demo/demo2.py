e0f1d3436c1e0e22b20e39728fee85348cb72060
Liam Cawley <47256354+cawley@users.noreply.github.com>
2023-06-13 12:52:28 -0400

    demo updates

--- rl_attempt/ql_demo/demo2.py
+++ rl_attempt/ql_demo/demo2.py
@@ -1,31 +1,29 @@
 import time
-import gym
-from matplotlib import pyplot as plt
+import gymnasium as gym
 
-env = gym.make("MountainCar-v0")
-env.render(mode = "human")
+env = gym.make("LunarLander-v2", render_mode = "human")
+env.reset()
 
-num_steps = 1500
+obs, info = env.reset(seed=42)
 
-obs = env.reset()
-
-for i in range(num_steps):
-    action = 2
+for i in range(1500):
+    # action = 1
+    # action = 2
     # intelligent action is
     # action = my_intelligent_agent_fn(obs)
     # random is
-    # action = env.action_space.sample()
+    action = env.action_space.sample()
 
     # do action
-    obs, reward, done, info = env.step(action)
+    obs, reward, terminated, truncated, info = env.step(action)
 
     # render env
     env.render()
 
     # slow vid down
-    time.sleep(.001)
+    # time.sleep(.001)
 
-    if done:
-        env.reset()
+    if terminated or truncated:
+       obs, info = env.reset()
 
 env.close()
