3140c358b911c8ef065917cd2e2c3aa8c6ce4db4
Liam Cawley <47256354+cawley@users.noreply.github.com>
2023-06-21 16:45:57 -0400

    xgb demo

--- regres/xgb_demo.py
+++ regres/xgb_demo.py
@@ -1,12 +1,28 @@
-from xgboost import XGBClassifier
-# read data
-from sklearn.datasets import load_iris
+import xgboost as xgb
+from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
-data = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(data['data'], data['target'], test_size=.2)
-# create model instance
-bst = XGBClassifier(n_estimators=2, max_depth=2, learning_rate=1, objective='binary:logistic')
-# fit model
-bst.fit(X_train, y_train)
-# make predictions
-preds = bst.predict(X_test)
+
+# Generate a binary classification dataset
+X, y = make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=10, n_classes=2, random_state=42)
+
+# Split the dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Convert the dataset into DMatrix which is a data structure that xgboost can use
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, label=y_test)
+
+# Define the parameters for xgboost
+param = {
+    'max_depth': 3,
+    'eta': 0.3,
+    'objective': 'binary:logistic',
+    'eval_metric': 'auc'
+}
+
+# Train the model
+num_round = 20
+bst = xgb.train(param, dtrain, num_round)
+
+# Make predictions
+preds = bst.predict(dtest)
