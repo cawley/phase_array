096da24484f4cd5ca74d26516f2c318334a0e4aa
Liam Cawley <47256354+cawley@users.noreply.github.com>
2023-06-30 14:10:59 -0400

    formatting updates

--- reinforcement_learning/dqn_demo/master.py
+++ reinforcement_learning/dqn_demo/master.py
@@ -1,14 +1,14 @@
 from func import DeepQLearning
 import gym
 
-env=gym.make('CartPole-v1')
+env = gym.make("CartPole-v1")
 
-gamma=1
-epsilon=0.1
-numberEpisodes=1000
+gamma = 1
+epsilon = 0.1
+numberEpisodes = 10
 
 # create an object
-LearningQDeep=DeepQLearning(env,gamma,epsilon,numberEpisodes)
+LearningQDeep = DeepQLearning(env, gamma, epsilon, numberEpisodes)
 # run the learning process
 LearningQDeep.trainingEpisodes()
 # get the obtained rewards in every episode
@@ -16,9 +16,6 @@ LearningQDeep.sumRewardsEpisode
 
 #  summarize the model
 LearningQDeep.mainNetwork.summary()
-# save the model, this is important, since it takes long time to train the model 
+# save the model, this is important, since it takes long time to train the model
 # and we will need model in another file to visualize the trained model performance
 LearningQDeep.mainNetwork.save("trained_model_temp.h5")
-
-
-
